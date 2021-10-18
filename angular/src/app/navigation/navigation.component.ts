3565de5cf5726ea18ad25f4e99fd98687b892dac
Bruno <bruno.kirchmeier@gmail.com>
2021-10-18 18:26:15 +0200

    Update: Navigation

--- angular/src/app/navigation/navigation.component.ts
+++ angular/src/app/navigation/navigation.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Router } from '@angular/router';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-navigation',
@@ -11,66 +8,33 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class NavigationComponent implements OnInit {
 
-  public loading: boolean = false;
-  public isAuthenticated: boolean = false;
   public title: string = "";
-  public isBypass: boolean = false;
-  public mobile: boolean = false;
-  public isMenuInitOpen: boolean = false;
 
-  constructor(private breakpointObserver: BreakpointObserver,
-              private router: Router,
-              private _snackBar: MatSnackBar,
-              private sidenav: MatSidenavModule) { }
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
-    public isMenuOpen = true;
-    public contentMargin = 240;
-    public isHandset: boolean = false;
+  public isMenuOpen = true;
+  public contentMargin = 240;
+  public isHandset: boolean = false;
 
-
-  // *********************************************************************************************
-  // * LIFE CYCLE EVENT FUNCTIONS
-  // *********************************************************************************************
-
-    ngOnInit() {
-      this.isMenuOpen = true;  // Open side menu by default
-      this.title = 'Material Layout Demo';
-      this.breakpointObserver.observe(Breakpoints.Handset)
-      .subscribe((state: BreakpointState) => {
-        if (state.matches) {
-          this.isHandset = true;
-        } else {
-          this.isHandset = false;
-        }
-      });
-    }
-
-    ngDoCheck() {
-        if (this.isHandset) {
-          this.isMenuOpen = false;
-        } else {
-          this.isMenuOpen = true;
-        }
-    }
-
-  // *********************************************************************************************
-  // * COMPONENT FUNCTIONS
-  // *********************************************************************************************
-
-  public openSnackBar(msg: string): void {
-    this._snackBar.open(msg, 'X', {
-      duration: 2000,
-      horizontalPosition: 'center',
-      verticalPosition: 'top',
-      panelClass: 'notif-error'
+  ngOnInit() {
+    this.isMenuOpen = true;  // Open side menu by default
+    this.title = 'Material Layout Demo';
+    this.breakpointObserver.observe(Breakpoints.Handset)
+    .subscribe((state: BreakpointState) => {
+      if (state.matches) {
+        this.isHandset = true;
+      } else {
+        this.isHandset = false;
+      }
     });
   }
 
-  public onSelectOption(option: any): void {
-    const msg = `Chose option ${option}`;
-    this.openSnackBar(msg);
-
-    /* To route to another page from here */
-    // this.router.navigate(['/home']);
+  ngDoCheck() {
+      if (this.isHandset) {
+        this.isMenuOpen = false;
+      } else {
+        this.isMenuOpen = true;
+      }
   }
+
 }
