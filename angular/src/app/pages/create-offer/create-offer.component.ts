1f324b45fdb9a4c70c334a4f73148775d77161cb
Bruno <bruno.kirchmeier@gmail.com>
2021-10-25 22:30:38 +0200

    Add: Angebot erstellen, Formular erstellen

--- angular/src/app/pages/create-offer/create-offer.component.ts
+++ angular/src/app/pages/create-offer/create-offer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ICardDetails } from '../../api/api-scryfall.service';
+
 
 @Component({
   selector: 'app-create-offer',
@@ -7,9 +10,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateOfferComponent implements OnInit {
 
+ ////////////////////////////////////////////////////////////////////////////////////////////////
+  // Declarations
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+
+  public cardDetailsList: ICardDetails[] = [];
+  public creatOfferisHidden: boolean = false;
+  public imgIsResized: boolean = false;
+  public creatOfferIcon: string = 'add';
+  public offerForm = new FormGroup({
+    offerPrice: new FormControl(''),
+    deliveryMode: new FormControl(''),
+  });
+  public deliveryModes = [{name: 'collection', description: 'Abholung'},
+                          {name: 'shipping', description: 'Versand'}]
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+  // Constructor and destructor
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  ngOnDestroy(): void {
+  }
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+  // Form Search (Search Option for API Request to Scryfall)
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+
+  onSubmit(): void {
+
+  }
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+  // Event functions
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+
+  resultsChanged(elements: ICardDetails[]) {
+    this.cardDetailsList = elements;
+  }
+
+  createOffer() {
+    this.creatOfferisHidden = !this.creatOfferisHidden;
+    this.creatOfferIcon = this.creatOfferisHidden === false
+                        ? 'add'
+                        : 'clear';
+  }
+
+  resizeImg() {
+    this.imgIsResized = !this.imgIsResized;
+  }
+
 
 }
