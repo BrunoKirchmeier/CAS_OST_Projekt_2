644c87325a3ccea7b1fd5c9d7503caf2abeb1fc9
BrunoKirchmeier <bruno.kirchmeier@gmail.com>
2021-11-21 22:42:33 +0100

    Update environment.prod.ts

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,11 +1,2 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyDv5ehDxsCi45NRCUyoDhRauZofyolWEEs",
-    authDomain: "magictauschboerse.firebaseapp.com",
-    projectId: "magictauschboerse",
-    storageBucket: "magictauschboerse.appspot.com",
-    messagingSenderId: "1096709073170",
-    appId: "1:1096709073170:web:b7f6c15727b13de1ed2bcb"
-  }
 };
