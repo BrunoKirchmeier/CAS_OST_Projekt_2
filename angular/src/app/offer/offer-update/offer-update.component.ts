6b3c159af2faa3f256f24ff0681cd4a463450159
Bruno <bruno.kirchmeier@gmail.com>
2021-11-28 23:20:26 +0100

    Add: View for Update Offer

--- angular/src/app/offer/offer-update/offer-update.component.ts
+++ angular/src/app/offer/offer-update/offer-update.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { BehaviorSubject, Subject, Subscription } from 'rxjs';
-import { IOffer, OfferService } from '../shared/services/offer.service';
+import { IDeliveryModes, IOffer, IPaymentModes, OfferService } from '../shared/services/offer.service';
 
 @Component({
   selector: 'app-offer-update',
@@ -13,26 +14,32 @@ export class OfferUpdateComponent implements OnDestroy {
   private _subscriptions: Subscription[] = [];
 
   public offerList$: Subject<IOffer[]> = new Subject();
+  public activeOfferDetails: string = '';
+  public form = new FormGroup({
+    offerPrice: new FormControl('', [Validators.required]),
+    deliveryMode: new FormControl('', [Validators.required]),
+    paymentMode: new FormControl('', [Validators.required]),
+    cardAmount: new FormControl('', [Validators.required]),
+    additionInfo: new FormControl('')
+  });
+  public deliveryModes: Array<IDeliveryModes> = [];
+  public paymentModes: Array<IPaymentModes> = [];
 
   constructor(private _offerService: OfferService,
               private _snackBar: MatSnackBar) {
+    this.deliveryModes = this._offerService.getDeliveryModes();
+    this.paymentModes = this._offerService.getPaymentModes();
   }
 
   ngOnInit() {
     this._subscriptions.push(
       this.offerList$
-        .subscribe(
-
-
-
-        )
+        .subscribe()
     );
-
     this._subscriptions.push(
       this._offerService.onChangeOwnerOffer$.
         subscribe((res) => {
           this.offerList$.next(res);
-            console.log('NEUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU');
         })
     )
   }
@@ -43,8 +50,23 @@ export class OfferUpdateComponent implements OnDestroy {
     });
   }
 
-  showOfferDetails() {
-    console.log('TEST');
+  showOfferDetails(value: IOffer) {
+    this.activeOfferDetails = value.cardDetails !== null && value.cardDetails !== undefined &&
+                              value.cardDetails._id !== null && value.cardDetails._id !== undefined
+                            ? value.cardDetails._id
+                            : '';
+  }
+
+  async onSubmit(): Promise<any> {
+    if(this.form.valid) {
+      const unitPrice = this.form.get('offerPrice')?.value;
+      const quantity = this.form.get('cardAmount')?.value;
+      const deliveryMode = this.form.get('deliveryMode')?.value;
+      const paymentMode = this.form.get('paymentMode')?.value;
+      const additionInfo = this.form.get('additionInfo')?.value;
+
+
+    }
   }
 
   closeSnackBar() {
@@ -52,7 +74,3 @@ export class OfferUpdateComponent implements OnDestroy {
   }
 
 }
-
-
-
-// onAddOwnerOffer$
