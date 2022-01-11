9cc5089d9b75f64ef06f861a67c4dfe14b9935c4
Bruno <bruno.kirchmeier@gmail.com>
2022-01-11 20:42:09 +0100

    Update: Warenkorb - Preis aktualisierung

--- angular/src/app/basket/basket-manage/basket-manage.component.ts
+++ angular/src/app/basket/basket-manage/basket-manage.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Subject } from 'rxjs';
 import { IBasket, BasketService } from '../shared/basket.service ';
 
@@ -7,40 +8,37 @@ import { IBasket, BasketService } from '../shared/basket.service ';
   templateUrl: './basket-manage.component.html',
   styleUrls: ['./basket-manage.component.scss']
 })
-export class BasketManageComponent implements OnInit, OnDestroy {
+export class BasketManageComponent implements OnInit {
 
-  private basketObj: {[id: string]: IBasket[] } = {};
+  private _basketObj: {[id: string]: IBasket[] } = {};
 
   public basketObj$: Subject<{[id: string]: IBasket[] }> = new Subject();
   public priceTotal: number = 0;
   public keyCount: number = 0;
 
-  constructor(private _basketService: BasketService) {
+  constructor(private _basketService: BasketService,
+              private _snackBar: MatSnackBar) {}
+
+	ngOnInit(): void {
     this.getBasket();
   }
-
-	ngOnInit(): void {}
-
-  ngOnDestroy(): void {}
 
   getBasket() {
     this.priceTotal = 0;
     this._basketService.getBasket()
       .then((res) => {
+        this._basketObj = res;
         this.keyCount = (Object.keys(res).length) -1;
         for(let key in res) {
           res[key].forEach((item: IBasket) => {
             if(this.checkOfferAvailability(item) === true) {
               this.priceTotal += item.offerDetail.cardPrice * item.quantity;
             }
-            if(item.providerDetail?.lastName == '' &&
-            item.providerDetail?.firstName == '') {
-              item.providerDetail.lastName = 'Anonym';
-            }
           })
         }
+        this.calcPriceTotal();
+        this._basketObj = res;
         this.basketObj$.next(res);
-        this.basketObj = res;
       })
   }
 
@@ -48,9 +46,9 @@ export class BasketManageComponent implements OnInit, OnDestroy {
     let nodeFocus: HTMLElement | null  = document.querySelector('.item-header-focus[id="' + item._id + '"');
     let nodeDetail: HTMLElement | null  = document.querySelector('.item-header-detail[id="' + item._id + '"');
     if(nodeFocus !== null &&
-      nodeDetail !== null ) {
-        nodeFocus.style.display = 'flex';
-        nodeDetail.style.display = 'none';
+       nodeDetail !== null ) {
+      nodeFocus.style.display = 'flex';
+      nodeDetail.style.display = 'none';
     }
   }
 
@@ -58,63 +56,67 @@ export class BasketManageComponent implements OnInit, OnDestroy {
     let nodeFocus: HTMLElement | null  = document.querySelector('.item-header-focus[id="' + item._id + '"');
     let nodeDetail: HTMLElement | null  = document.querySelector('.item-header-detail[id="' + item._id + '"');
     if(nodeFocus !== null &&
-      nodeDetail !== null ) {
-        nodeFocus.style.display = 'none';
-        nodeDetail.style.display = 'flex';
+       nodeDetail !== null ) {
+      nodeFocus.style.display = 'none';
+      nodeDetail.style.display = 'flex';
     }
   }
 
-  quantityAdd(item: IBasket) {
-    let node: HTMLSpanElement | null = document.querySelector<HTMLSpanElement>('.item-quantity-value[id="' + item._id + '"');
+  quantityAdd(htmlItem: IBasket) {
+    let node: HTMLSpanElement | null = document.querySelector<HTMLSpanElement>('.item-quantity-value[id="' + htmlItem._id + '"');
     let htmlValue: string = node?.innerText ?? '0';
     let htmlQuantity: any = parseFloat(htmlValue);
-    let itemQuantity: number = item?.offerDetail?.quantity ?? 0;
-    if(node !== null &&
-       htmlQuantity < itemQuantity) {
+    let varItem: IBasket | undefined = this._basketObj[htmlItem.providerDetail.uid].find(obj => obj.offerId === htmlItem.offerId);
+    if(varItem !== undefined &&
+       node !== null &&
+       htmlQuantity < varItem.offerDetail.quantity) {
       htmlQuantity++;
       node.innerHTML = htmlQuantity.toString();
     }
-    node = document.querySelector<HTMLSpanElement>('.item-details-price[id="' + item._id + '"');
-    let price = htmlQuantity * (item.offerDetail?.cardPrice ?? 0);
-    if(node !== null) {
+    node = document.querySelector<HTMLSpanElement>('.item-details-price[id="' + htmlItem._id + '"');
+    if(varItem !== undefined &&
+       node !== null) {
+      let price = htmlQuantity * (varItem.offerDetail?.cardPrice ?? 0);
       node.innerHTML = (Math.ceil(price*20)/20).toFixed(2) + ' CHF';
     }
-    item.quantity = htmlQuantity;
-    this.updateItem(item);
-    this.reRenderingPriceTotal();
-    const index: number = this.basketObj[item.providerDetail.uid].findIndex(obj => obj.offerId === item.offerId);
-    this.basketObj[item.providerDetail.uid][index].quantity = htmlQuantity;
+    if(varItem !== undefined) {
+       varItem.quantity = htmlQuantity;
+      this.updateItem(varItem);
+    }
+    this.calcPriceTotal();
   }
 
-  quantityRemove(item: IBasket) {
-    let node: HTMLSpanElement | null = document.querySelector<HTMLSpanElement>('.item-quantity-value[id="' + item._id + '"');
+  quantityRemove(htmlItem: IBasket) {
+    let node: HTMLSpanElement | null = document.querySelector<HTMLSpanElement>('.item-quantity-value[id="' + htmlItem._id + '"');
     let htmlValue: string = node?.innerText ?? '0';
     let htmlQuantity: number = parseFloat(htmlValue);
+    let varItem: IBasket | undefined = this._basketObj[htmlItem.providerDetail.uid].find(obj => obj.offerId === htmlItem.offerId);
     if(node !== null &&
        htmlQuantity > 0) {
       htmlQuantity--;
       node.innerText = htmlQuantity.toString();
     }
-    node = document.querySelector<HTMLSpanElement>('.item-details-price[id="' + item._id + '"');
-    let price = htmlQuantity * (item.offerDetail?.cardPrice ?? 0);
-    if(node !== null) {
-      node.innerText = (Math.ceil(price*20)/20).toFixed(2) + ' CHF';
+    node = document.querySelector<HTMLSpanElement>('.item-details-price[id="' + htmlItem._id + '"');
+    if(varItem !== undefined &&
+       node !== null) {
+      let price = htmlQuantity * (varItem.offerDetail?.cardPrice ?? 0);
+      node.innerHTML = (Math.ceil(price*20)/20).toFixed(2) + ' CHF';
     }
-    item.quantity = htmlQuantity;
-    this.updateItem(item);
-    this.reRenderingPriceTotal();
-    const index: number = this.basketObj[item.providerDetail.uid].findIndex(obj => obj.offerId === item.offerId);
-    this.basketObj[item.providerDetail.uid][index].quantity = htmlQuantity;
+    if(varItem !== undefined) {
+       varItem.quantity = htmlQuantity;
+    this.updateItem(varItem);
+    }
+    this.calcPriceTotal();
   }
 
-  reRenderingPriceTotal() {
+  calcPriceTotal() {
     this.priceTotal = 0;
-    let nodeListPrices = document.querySelectorAll<HTMLSpanElement>('.item-details-price');
-    nodeListPrices.forEach((node: HTMLSpanElement ) => {
-      let htmlValue: string = node?.innerText ?? '0';
-      let htmlQuantity: number = parseFloat(htmlValue);
-      this.priceTotal += htmlQuantity;
-    });
+    this.keyCount = (Object.keys(this._basketObj).length) -1;
+    for(let key in this._basketObj) {
+      this._basketObj[key].forEach((item: IBasket) => {
+        this.priceTotal += item.quantity * item.offerDetail.cardPrice;
+      })
+    }
   }
 
   removeItem(item: IBasket) {
@@ -125,37 +127,72 @@ export class BasketManageComponent implements OnInit, OnDestroy {
   }
 
   updateItem(item: IBasket) {
-    this._basketService.updateItem(item)
+    const index: number = this._basketObj[item.providerDetail.uid].findIndex(obj => obj.offerId === item.offerId);
+    if(index >= 0) {
+      this._basketObj[item.providerDetail.uid][index] = item;
+      this._basketService.updateItem(item)
+    }
   }
 
-  checkOfferAvailability(item: IBasket): any {
+  checkOfferAvailability(item: IBasket): boolean {
     let ret: Boolean = false;
     let nodeQuantity: HTMLSpanElement | null = document.querySelector<HTMLSpanElement>('.item-quantity-value[id="' + item._id + '"');
     let htmlValue: string = nodeQuantity?.innerText ?? '0';
     let htmlQuantity: number = parseFloat(htmlValue);
+    let varItem: IBasket | undefined = undefined;
+    const index: number = this._basketObj[item.providerDetail.uid].findIndex(obj => obj.offerId === item.offerId);
+    varItem = this._basketObj[item.providerDetail.uid][index];
     // good
-    if(item.offerDetail.quantity >= htmlQuantity &&
-        item.offerDetail.quantity >= item.quantity) {
+    if(varItem !== undefined &&
+      varItem.offerDetail.quantity >= htmlQuantity &&
+      varItem.offerDetail.quantity >= varItem.quantity) {
       ret = true;
-    } else if(nodeQuantity !== null) {
-      nodeQuantity.innerText = item.offerDetail.quantity.toString();
-      let nodeItem: HTMLDivElement | null = document.querySelector<HTMLDivElement>('.item-details[id="' + item._id + '"');
-      if(nodeItem !== null) {
-        nodeItem.style.color = 'red';
+      if(nodeQuantity !== null) {
+        nodeQuantity.classList.remove('item-quanitity-error');
       }
+    } else if(varItem !== undefined &&
+              nodeQuantity !== null) {
+      nodeQuantity.classList.add('item-quanitity-error');
+      nodeQuantity.innerText = varItem.offerDetail.quantity.toString();
+      this._basketObj[item.providerDetail.uid][index].quantity = varItem.offerDetail.quantity;
     }
-    return ret;
+    return ret === true ? true: false;
   }
 
   checkBasket() {
     this._basketService.getBasket()
     .then((res) => {
+      this._basketObj = res;
+      let isFailed: Boolean = false;
       for(let key in res) {
-        res[key].forEach((item: IBasket) => {
-          this.checkOfferAvailability(item);
+        res[key].forEach((varItem: IBasket) => {
+          if(this.checkOfferAvailability(varItem) === false) {
+            isFailed = false;
+            let dbItem: IBasket | undefined = this._basketObj[varItem.providerDetail.uid].find(obj => obj.offerId === varItem.offerId);
+            let node: HTMLSpanElement | null = document.querySelector<HTMLSpanElement>('.item-details-price[id="' + dbItem?._id + '"');
+            if(dbItem !== undefined &&
+               node !== null) {
+                let price = dbItem.quantity * (dbItem.offerDetail?.cardPrice ?? 0);
+                node.innerText = (Math.ceil(price*20)/20).toFixed(2) + ' CHF';
+            }
+            if(dbItem !== undefined) {
+              this.updateItem(dbItem);
+            }
+          }
         })
       }
+      this.calcPriceTotal();
+      if(isFailed === false){
+        this._snackBar.open('Die Stückzahl mindestens eines Artikels wurde auf die maximal verfübare Menge reduziert');
+          setTimeout(() => {
+            this._snackBar.dismiss();
+          }, 3000)
+      }
     })
+  }
+
+  closeSnackBar() {
+    this._snackBar.dismiss();
   }
 
 }
